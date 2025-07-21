6b1358a8f69cf6a7f182585e0731899cd588b185
Natallia Paliukhovich-Karastsialiova <n.paliukhovich98@gmail.com>
2025-07-21 18:07:42 +0500

    feat: implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const positiveNumStr = Math.abs(n).toString();
+  let result = '';
+  for (let i = positiveNumStr.length - 1; i >= 0; i -= 1) {
+    result += positiveNumStr[i];
+  }
+  return result;
 };
